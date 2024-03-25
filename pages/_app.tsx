60b659871e590be6c06662b8c9b16567e31941a2
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-03-26 03:29:42 +0900

    hotfix : Main 브랜치 작업 중 잘못 지정한 부분 검토 중

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import "nextra-theme-docs/style.css";
+import Head from "next/head";
+
+export default function Nextra({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS"
+          href="/feed.xml"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
